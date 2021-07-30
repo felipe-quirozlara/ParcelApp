d6ba19b6e14cb002a0b583816001c3f5673a66d9
Felipe Quiroz <felipe.i.quiroz.lara@gmail.com>
2021-07-30 12:33:08 -0400

    Modelos agregados

--- parcel/admin.py
+++ parcel/admin.py
@@ -3,3 +3,4 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Envio)
+admin.site.register(models.ListaPrecio)
--- parcel/models.py
+++ parcel/models.py
@@ -19,8 +19,27 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre + ' ' + str(self.envio)
 
-class Direccion(models.Model):
+class DireccionEnvio(models.Model):
     envio = models.ForeignKey(Envio, on_delete=models.CASCADE)
     calle = models.CharField(max_length=250)
     comuna = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.envio.id + ' ' + self.id + '' + self.calle)
+
+class Retiro(models.Model):
+    envio = models.ForeignKey(Envio, on_delete=models.CASCADE)
+    calle = models.CharField(max_length=250)
+    comuna = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.envio.id + ' ' + self.id + '' + self.calle)
+
+class ListaPrecio(models.Model):
+    distancia = models.IntegerField()
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id + ' ' + self.precio)
+
 
